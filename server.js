1a70d3f0cf1f7fd954a4edee665a9c9a261b0717
Adrian Babieczko <adrian.babieczko@gmail.com>
2021-08-05 22:36:28 +0200

    Hide console logs

--- server.js
+++ server.js
@@ -15,14 +15,14 @@ io.on('connection', (socket) => {
     console.log('New client! Its id – ' + socket.id);
 
     socket.on('message', (message) => {
-        console.log('Oh, I\'ve got something from ' + socket.id);
+        // console.log('Oh, I\'ve got something from ' + socket.id);
         messages.push(message);
         socket.broadcast.emit('message', message);
     });
 
     socket.on('disconnect', () => { console.log('Oh, socket ' + socket.id + ' has left') });
 
-    console.log('I\'ve added a listener on message and disconnect events \n');
+    // console.log('I\'ve added a listener on message and disconnect events \n');
 });
 
 app.use(express.static('client'))
